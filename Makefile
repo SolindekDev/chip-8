b2a617ea6392c577a2b2b47bbb2b3772d386fa05
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2022-01-13 16:15:16 +0100

    Added base

---
+++ Makefile
@@ -0,0 +1,7 @@
+all: main run
+
+main:
+	gcc main.c
+
+run:
+	./a.exe
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int memory[1024];
+unsigned int registers[16];
+unsigned int stack[];
+
+int main(int argc, char **argv) {
+    return 0;
+}
